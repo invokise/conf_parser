d269faad9f346666b08e82e215eb4f391a6b9865
invokise <bilol586746@mail.ru>
2021-12-15 19:46:37 +0300

    true

--- conf_parser.py
+++ conf_parser.py
@@ -1,82 +1,78 @@
+"""
+<s-exp-list> ::= <s-exp> <s-exp-list> | <empty>
+<s-exp> ::=  "(" + name + <data> + ")"
+<name> ::= name | empty
+<data> ::= <string> | <integer> | <s-exp> 
+
+"""
+import sys
 import json
 from pathlib import Path
-from collections import namedtuple
+from pprint import pprint
 from sly import Lexer, Parser
 
-
-
-
-
-Directive = namedtuple("Direrctive", "type args ctx")
-
-class ConfLexer(Lexer): 
-
-    # Set of token names.   This is always required
-    tokens = {BEGIN, END, NAME, SEMICOL, STRING}
-
-
-    # Regular expression rules for tokens
-    BEGIN = r'{'
-    END = r'}'
+class ConfLexer(Lexer):
+    tokens = {BEGIN, END, NAME, INTEGER, STRING}
+    BEGIN = r'\('
+   
     ignore = r' \t'
-    ignore_comment = r'\#.*'
-    ignore_newline = r'\n+'
-    NAME = r'[^ \t\#{};\']+'
-    SEMICOL = r';'
-    STRING = r"'.*'"
+    ignore_newline = r'\n'
+    NAME = r'[^ \(\)0123456789\t\nйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]+'
+    STRING = r'[^\(\)\t\#\nqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]+'
+    INTEGER = r'[0123456789]+'
+    
+   
+    END = r'\)' 
 
 
 class ConfParser(Parser):
     tokens = ConfLexer.tokens
 
-    @_('directive directives')
-    def directives(self, p):
-        return [p.directive] + p.directives
+    @_('sexp sexp sexp')
+    def sexplist(self, p):
+        return [p.sexp0] + [p.sexp1]
 
-
-    @_('empty')
-    def directives(self, p):
-        return []
-
-
-    @_('simple_directive', 'block_directive')
-    def directive(self, p):
-        return p[0]
-
-
-    @_('name names SEMICOL')
-    def simple_directive(self, p):
-        # return Directive("simple", [p.NAME] + p.names, [])
-        return dict(type="simple", args=[p.name] + p.names, ctx=[])
-
-    @_('name names BEGIN directives END')
-    def block_directive(self, p):
-        # return Directive("block", [p.NAME] + p.names, p.directives)
-        return dict(type = "block", args=[p.name] + p.names, ctx = p.directives)
-
-
-    @_('name names')
-    def names(self, p):
-        return [p.name] + p.names   
-
+    @_('BEGIN NAME sexp sexplist END')
+    def sexp(self, p):
+        return dict(args=[p.NAME] + [p.sexp] + p.sexplist)
+   
+    @_('BEGIN sexp sexplist END')
+    def sexp(self, p):
+        return dict(args=[p.sexp] + p.sexplist)
+    
+    @_('BEGIN NAME data END')
+    def sexp(self, p):
+        return dict(args=[p.NAME] + [p.data])
+    
+    @_('sexp sexplist')
+    def sexplist(self, p):
+        return [p.sexp] + p.sexplist
 
     @_('empty')
-    def names(self, p):
+    def sexplist(self, p):
         return []
+    
+    @_('STRING')
+    def data(self, p):
+        return [p.STRING]
 
-    @_('NAME', 'STRING')
-    def name(self, p):
-        return p[0]
+    @_('INTEGER')
+    def data(self, p):
+        return [p.INTEGER]
 
+    @_('empty')
+    def data(self, p):
+        return []
 
     @_('')
     def empty(self, p):
         pass
 
-
-text = Path("nginx.conf").read_text()
+arg = sys.argv[1]
+file = open(str(arg), 'r')
+text = file.read()
 lexer = ConfLexer()
 parser = ConfParser()
 data = parser.parse(lexer.tokenize(text))
-print(data)
-Path("result.json").write_text(json.dumps(data, indent= 2))
+Path("result.json").write_text(json.dumps(data, indent=2, ensure_ascii= False))
+pprint(parser.parse(lexer.tokenize(text)))
