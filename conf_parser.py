bab76792a7d5d223b8dd519c7d65f3767e840f7e
invokise <bilol586746@mail.ru>
2021-12-22 17:53:23 +0300

    first commit

--- conf_parser.py
+++ conf_parser.py
@@ -2,74 +2,83 @@
 <s-exp-list> ::= <s-exp> <s-exp-list> | <empty>
 <s-exp> ::=  "(" + name + <data> + ")"
 <name> ::= name | empty
-<data> ::= <string> | <integer> | <s-exp> 
-
+<data> ::= <string> | <number> | <s-exp> 
+ 
 """
+from json.encoder import JSONEncoder
 import sys
 import json
 from pathlib import Path
 from pprint import pprint
 from sly import Lexer, Parser
-
+ 
+counter = 0
+ 
 class ConfLexer(Lexer):
-    tokens = {BEGIN, END, NAME, INTEGER, STRING}
+    tokens = {BEGIN, END, NAME, NUMBER, STRING}
     BEGIN = r'\('
-    END = r'\)' 
-    NAME = r'[^ \(\)0123456789\t\nйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]+'
-    STRING = r'[^\(\)\t\#\nqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]+'
-    INTEGER = r'[0123456789]+'
+ 
     ignore = r' \t'
     ignore_newline = r'\n'
-
-
+    NAME = r'[^ \(\)0123456789\t\nйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ]+'
+    NUMBER = r'[0123456789]+'
+    STRING = r'[^\(\)\t\#\nqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM]+'
+ 
+    END = r'\)' 
+ 
 class ConfParser(Parser):
     tokens = ConfLexer.tokens
-
+ 
     @_('sexp sexp sexp')
     def sexplist(self, p):
-        return [p.sexp0] + [p.sexp1]
-
+        return [p.sexp0] + [p.sexp1] + [p.sexp2]
+ 
     @_('BEGIN NAME sexp sexplist END')
     def sexp(self, p):
-        return dict(args=[p.NAME] + [p.sexp] + p.sexplist)
-   
+        return [p.NAME] + [p.sexp] + p.sexplist
+ 
     @_('BEGIN sexp sexplist END')
     def sexp(self, p):
-        return dict(args=[p.sexp] + p.sexplist)
-    
+        return [p.sexp] + p.sexplist
+ 
     @_('BEGIN NAME data END')
     def sexp(self, p):
-        return dict(args=[p.NAME] + [p.data])
-    
+        global counter
+        counter += 1
+        if (counter < 62):
+              return [p.data]
+        else:
+              return [p.NAME] + [p.data]
+ 
     @_('sexp sexplist')
     def sexplist(self, p):
         return [p.sexp] + p.sexplist
-
+ 
     @_('empty')
     def sexplist(self, p):
         return []
-    
+ 
     @_('STRING')
     def data(self, p):
         return [p.STRING]
-
-    @_('INTEGER')
+ 
+    @_('NUMBER')
     def data(self, p):
-        return [p.INTEGER]
-
+        return [p.NUMBER]
+ 
     @_('empty')
     def data(self, p):
         return []
-
+ 
     @_('')
     def empty(self, p):
         pass
-
+ 
 arg = sys.argv[1]
 file = open(str(arg), 'r')
 text = file.read()
 lexer = ConfLexer()
 parser = ConfParser()
 data = parser.parse(lexer.tokenize(text))
-Path("result.json").write_text(json.dumps(data, indent=2, ensure_ascii= False))
+Path("result.json").write_text(json.dumps(data, indent=2, ensure_ascii=False))
 pprint(parser.parse(lexer.tokenize(text)))
